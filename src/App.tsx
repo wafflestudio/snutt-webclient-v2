6680cb8007481eb1f84ebedd5bbf390d5eabec24
KiWan KIM <dkwanm00@gmail.com>
2022-12-26 17:54:44 +0900

    eslint 및 절대경로 설정 (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Main } from "./pages/main";
-import { Login } from "./pages/login";
-import { SignUp } from "./pages/signup";
-import { MyPage } from "./pages/mypage";
-import { createGlobalStyle } from "styled-components";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+
+import { Login } from '@/pages/login';
+import { Main } from '@/pages/main';
+import { MyPage } from '@/pages/mypage';
+import { SignUp } from '@/pages/signup';
 
 const router = createBrowserRouter([
-  { path: "/", element: <Main /> },
-  { path: "/login", element: <Login /> },
-  { path: "/signup", element: <SignUp /> },
-  { path: "/mypage", element: <MyPage /> },
+  { path: '/', element: <Main /> },
+  { path: '/login', element: <Login /> },
+  { path: '/signup', element: <SignUp /> },
+  { path: '/mypage', element: <MyPage /> },
 ]);
 
 function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
 );
