4691dacad09ff6eab25cb7bfbce0c9d56ab76d8e
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2022-12-27 22:40:48 +0900

    ✨ 현재 시간표 강의 목록 보여주는 기능 구현 (#19)

--- src/entities/timetable.ts
+++ src/entities/timetable.ts
@@ -1,3 +1,5 @@
+import { Lecture } from './lecture';
+
 export interface Timetable {
   semester: 1 | 2 | 3 | 4;
   title: string;
@@ -8,36 +10,7 @@ export interface Timetable {
 }
 
 export interface FullTimetable {
-  lecture_list: {
-    _id: string;
-    classification?: string;
-    department?: string;
-    academic_year?: string;
-    course_title: string;
-    credit: number;
-    class_time?: string;
-    real_class_time: string;
-    class_time_json: {
-      _id: string;
-      day: number;
-      start: number;
-      len: number;
-      place: string;
-      start_time: string;
-      end_time: string;
-    }[];
-    class_time_mask: number[];
-    instructor: string;
-    quota: number;
-    remark: string;
-    category?: string;
-    course_number?: string;
-    lecture_number?: string;
-    created_at: string;
-    updated_at: string;
-    color: unknown;
-    colorIndex: number;
-  }[];
+  lecture_list: Lecture[];
   semester: 1 | 2 | 3 | 4;
   theme: number;
   title: string;
---
+++ src/pages/main/main-lecture-section/main-lecture-list-item/index.tsx
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+
+import { Lecture } from '@/entities/lecture';
+
+type Props = { lecture: Lecture };
+
+export const MainLectureListItem = ({ lecture }: Props) => {
+  return (
+    <LectureListItem data-testid="main-lecture-listitem">
+      <LectureInner>
+        <LectureHeader>
+          <LectureHeaderLeft>
+            <LectureTitle data-testid="main-lecture-listitem-title">{lecture.course_title}</LectureTitle>
+            <LectureInstructor data-testid="main-lecture-listitem-instructor">
+              {lecture.instructor} / {lecture.credit}학점
+            </LectureInstructor>
+          </LectureHeaderLeft>
+          <LectureNumber data-testid="main-lecture-listitem-number">
+            {lecture.course_number} / {lecture.lecture_number}
+          </LectureNumber>
+        </LectureHeader>
+        <LectureDepartment data-testid="main-lecture-listitem-department">
+          {lecture.department}, {lecture.academic_year}
+        </LectureDepartment>
+        <LectureTime data-testid="main-lecture-listitem-time">{lecture.real_class_time}</LectureTime>
+      </LectureInner>
+    </LectureListItem>
+  );
+};
+
+const LectureListItem = styled.li`
+  list-style-type: none;
+
+  &:hover {
+    background-color: #f8f8f9;
+  }
+`;
+
+const LectureInner = styled.div`
+  margin: 0 30px;
+  padding: 20px 0 14px;
+  border-bottom: 1px solid #ebeef2;
+`;
+
+const LectureHeader = styled.div`
+  justify-content: space-between;
+  display: flex;
+`;
+
+const LectureHeaderLeft = styled.div`
+  display: flex;
+`;
+
+const LectureTitle = styled.div`
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 700;
+`;
+
+const LectureInstructor = styled.div`
+  margin-left: 6px;
+  opacity: 0.6;
+  font-size: 13px;
+  line-height: 18px;
+`;
+
+const LectureNumber = styled.div`
+  opacity: 0.4;
+  font-size: 13px;
+  line-height: 18px;
+`;
+
+const LectureDepartment = styled.div`
+  opacity: 0.8;
+  margin: 10px 0 5px;
+  font-size: 14px;
+`;
+
+const LectureTime = styled.div`
+  opacity: 0.8;
+  font-size: 14px;
+`;
