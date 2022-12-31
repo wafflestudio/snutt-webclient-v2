75da5815173c8e43926855097b821866757c8b45
woohm402 <woohm404@gmail.com>
2022-12-31 15:50:08 +0900

    mypage 레이아웃 구현

--- e2e/mypage/mypage.spec.ts
+++ e2e/mypage/mypage.spec.ts
@@ -1,6 +1,17 @@
 import { expect, test } from '@playwright/test';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { givenUser } from '../utils/user.ts';
+
+test('로그인되지 않았으면 로그인으로 리다이렉트된다', async ({ page }) => {
+  await page.goto('/mypage');
+  await givenUser(page, { login: false });
+  await expect(page).toHaveURL('/login');
+});
+
 test('ui가 잘 보여진다', async ({ page }) => {
   await page.goto('/mypage');
-  await expect(page.getByText('mypage')).toHaveCount(1);
+  await givenUser(page, { login: true });
+  await expect(page.getByTestId('mypage-my-id')).toContainText('SNUTT 아이디는 woohm402입니다.');
 });
