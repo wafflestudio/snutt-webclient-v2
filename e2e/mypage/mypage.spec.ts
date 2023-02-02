5b36c79ce80ee0af1ac552126a12b74163ce1c6f
Hyunmin Woo <woohm404@gmail.com>
2023-02-02 20:42:24 +0900

    ✨ 페이스북 계정에 아이디 추가하기 기능 구현 (#118)

--- e2e/mypage/mypage.spec.ts
+++ e2e/mypage/mypage.spec.ts
@@ -25,7 +25,7 @@ test('local_id 가 없으면 facebook 관련 버튼이 노출되지 않는다.',
 test('facebook 연동을 해지하면 페이스북 연동하기 버튼이 노출된다.', async ({ page }) => {
   await page.goto('/mypage');
   await givenUser(page, { login: true, type: 'local_fb' });
-  const detachButton = await page.getByTestId('facebook-detach-button');
+  const detachButton = page.getByTestId('facebook-detach-button');
   await detachButton.click();
   await expect(page.getByTestId('facebook-attach-button')).toHaveCount(1);
 });
