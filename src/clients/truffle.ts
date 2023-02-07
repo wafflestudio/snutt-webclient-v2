919d3704dffb54632c367b4456cd91f78fda3798
woohm402 <woohm404@gmail.com>
2023-02-07 22:44:05 +0900

    truffle-browser v0.2.1

--- src/clients/truffle.ts
+++ src/clients/truffle.ts
@@ -3,7 +3,7 @@ import { getTruffleClient } from '@wafflestudio/truffle-browser';
 import { envService } from '@/usecases/envService';
 
 export const truffleClient = getTruffleClient({
-  enabled: envService.getNodeEnv() === 'production',
+  enabled: envService.getNodeEnv() === 'production' || true,
   app: { name: 'snutt-webclient', phase: envService.getAppEnv() },
   apiKey: envService.getTruffleApiKey(),
 });
